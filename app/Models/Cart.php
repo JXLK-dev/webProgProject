0a551fe884d4a5b952e6d28cd749ceba553086e4
valenciav <valencia.vananda.vv@gmail.com>
2022-12-17 12:05:51 +0700

    cart & cartdetail structure

---
+++ app/Models/Cart.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id'
+    ];
+    public function details(){
+        return $this->hasMany(CartDetail::class);
+    }
+    public function user(){
+        return $this->belongsTo(maiBoutique::class);
+    }
+    protected $table='carts';
+}
