7f92332114888bd6e5891fa4c424fea3f8087a82
valenciav <valencia.vananda.vv@gmail.com>
2023-01-04 02:44:26 +0700

    transaction

--- database/migrations/2023_01_03_093707_create_transaction_details_table.php
+++ database/migrations/2023_01_03_093707_create_transaction_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTransactionsTable extends Migration
+class CreateTransactionDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateTransactionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create('transaction_details', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('usercredentials')->onUpdate('cascade')->onDelete('cascade');
-            $table->unsignedBigInteger('transaction_id');
+            $table->unsignedBigInteger('item_id');
+            $table->foreign('item_id')->references('id')->on('itemdetails')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('quantity');
             $table->string('date');
             $table->timestamps();
         });
@@ -30,6 +32,6 @@ class CreateTransactionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('transaction_details');
     }
 }
